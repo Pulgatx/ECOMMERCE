3071a8bd5addd60d40d556e8304040321b3bf1a6
Cristian David Arango Torres <96159199+Pulgatx@users.noreply.github.com>
2023-07-08 19:44:11 -0500

    Update db.js

--- back_store/database/db.js
+++ back_store/database/db.js
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize'
 
 const db = new Sequelize('e-commerce','pulg4tx1','cristyan115',{
-    host:'www.xxxxxxxpulg4tx.com.mialias.net',
+    host:'xxxxxxxpulg4tx.com.mialias.net',
     dialect: 'mysql'
 })
 
